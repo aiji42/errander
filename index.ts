7f8c3afbb4a48dda095f1386028ce2df0786cc15
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-12-31 23:57:40 +0900

    add timeout

--- index.ts
+++ index.ts
@@ -5,18 +5,31 @@ const download = async (url: string, dir: string): Promise<void> => {
   await $`aria2c -d ${dir} --seed-time=0 --max-overall-upload-limit=1K ${url}`;
 };
 
+const timeout = async (time: number): Promise<string[]> => {
+  return new Promise((...[, reject]) => {
+    setTimeout(reject, time, new Error(`timeout (${time / 1000}s)`));
+  });
+};
+
 program
   .argument("<url>", "download url")
-  .option("-d, --directory <path>", "download directory path", "downloads");
+  .option("-d, --directory <path>", "download directory path", "downloads")
+  .option("-t, --timeout <seconds>", "timeout", String(30 * 60));
 
 program.parse(process.argv);
 
-const { directory } = program.opts<{ directory: string }>();
+const { directory, timeout: timeoutSec } = program.opts<{
+  directory: string;
+  timeout: string;
+}>();
 const [url] = program.args;
 new URL(url);
 
 const main = () => {
-  return download(url, directory);
+  return Promise.race([
+    download(url, directory),
+    timeout(Number(timeoutSec) * 1000),
+  ]);
 };
 
 main()
